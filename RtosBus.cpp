21cc1db38dfee849b5b0f16b8a8f71c8dcd102c3
vortex314 <lieven.merckx@gmail.com>
2018-02-05 22:44:55 +0100

    disable DWM1000 by default

--- RtosBus.cpp
+++ RtosBus.cpp
@@ -184,7 +184,7 @@ extern "C" void user_init(void)
     sdk_wifi_get_macaddr(STATION_IF, un.mac);
     Str role(2);
     config.setNameSpace("dwm1000");
-    config.get("role",role,"A");
+    config.get("role",role,"N");
 
     if ( role.peek(0)=='T' ) {
         DWM1000_Tag* tag=new DWM1000_Tag("tag",Spi::create(12,13,14,15),DigitalIn::create(4),DigitalOut::create(5));
--- load.sh
+++ load.sh
@@ -3,4 +3,5 @@ make flash TTY=USB1
 make flash TTY=USB2
 make flash TTY=USB3
 make flash TTY=USB4
+make flash TTY=USB5
 date
